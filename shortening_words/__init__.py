7737e93c43c706b711202eef56edb7bd55eedbe6
Fabricio Santolin da Silva <fabricio.santolin-da-silva@al-enterprise.com>
2021-09-13 13:29:02 +0200

    Add language option

--- shortening_words/__init__.py
+++ shortening_words/__init__.py
@@ -3,8 +3,8 @@ from ._version import get_versions
 from . import _version
 
 
-def shortit(word, length):
-    return sw(words=word, max_len=length).phrase
+def shortit(word, length, lang):
+    return sw(words=word, max_len=length, lang=lang).phrase
 
 
 __version__ = get_versions()['version']
